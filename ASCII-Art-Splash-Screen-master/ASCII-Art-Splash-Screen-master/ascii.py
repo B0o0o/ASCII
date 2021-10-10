13de966b003143f005087cb41e0ec46223227c20
BOO <91785671+B0o0o@users.noreply.github.com>
2021-10-10 13:11:28 +0300

    Update ascii.py

--- ASCII-Art-Splash-Screen-master/ASCII-Art-Splash-Screen-master/ascii.py
+++ ASCII-Art-Splash-Screen-master/ASCII-Art-Splash-Screen-master/ascii.py
@@ -1,5 +1,5 @@
 import random
 import os
 i = random.randrange(9) + 1 
-website="https://raw.githubusercontent.com/DanCRichards/ASCII-Art-Splash-Screen/master/art/"+ str(i) + ".txt"
+website="https://raw.githubusercontent.com/B0o0o/ASCII/ASCII-Art-Splash-Screen/master/art/"+ str(i) + ".txt"
 os.system("curl " +  website)
